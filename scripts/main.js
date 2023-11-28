b94c2357674a55a84240e9847351ad9f01eb9f6a
aditya <adityahongalwork@gmail.com>
2023-11-28 10:44:10 +0530

    LOGO CHANGES ON CLICKING

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,18 @@
-//const myHeading = document.querySelector("h1");
-//myHeading.textContent = "Hello world!";
+// CLICK EVENT
+//document.querySelector("html").addEventListener("click", function () {
+//    alert("Ouch! Stop poking me!");
+//  });
+
+// LOGO CHANGE ON CLICKING
+// const myImage = document.querySelector("img");
+
+// myImage.onclick = () => {
+//   const mySrc = myImage.getAttribute("src");
+//   if (mySrc === "images\firefoxlogo.png") {
+// myImage.setAttribute("src", "images\firefoxlogo2.png");
+//   } else {
+// myImage.setAttribute("src", "images\firefoxlogo.png");
+//   }
+// };
 
 
-// function multiply(num1, num2) {
-// return num1 * num2;
-// }
-
-document.querySelector("html").addEventListener("click", function () {
-    alert("Ouch! Stop poking me!");
-  });
