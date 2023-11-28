50e78c8f1b3d53729bb6ea4d65ed2ad5a87637b0
aditya <adityahongalwork@gmail.com>
2023-11-28 10:32:27 +0530

    added click event in js file

---
+++ scripts/main.js
@@ -0,0 +1,11 @@
+//const myHeading = document.querySelector("h1");
+//myHeading.textContent = "Hello world!";
+
+
+// function multiply(num1, num2) {
+// return num1 * num2;
+// }
+
+document.querySelector("html").addEventListener("click", function () {
+    alert("Ouch! Stop poking me!");
+  });
